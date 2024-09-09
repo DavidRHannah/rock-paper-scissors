7bac6af8d6b96effb6974fcf90d483db47b58fd5
David Hannah <dhann013@ucr.edu>
2024-09-08 18:59:30 -0700

    add game logic and ui

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
+let humanScore      = 0;
+let computerScore   = 0;
 
-//playGame(5)
+output = document.getElementById("output-text");
+humanScoreText = document.getElementById("human-score-span");
+computerScoreText = document.getElementById("computer-score-span");
 
-function playGame(roundAmnt){
-    let humanScore = 0
-    let computerScore = 0
+rockBtn = document.getElementById("rock");
+paperBtn = document.getElementById("paper");
+scissorsBtn = document.getElementById("scissors");
 
-    for (let i = 1; i <= roundAmnt; i++){
-        let computerChoice = getComputerChoice()
-        let humanChoice = getHumanChoice()
-        let roundResult = playRound(humanChoice, computerChoice)
-        console.log(roundResult)
+rockBtn.addEventListener("click", ()=>playRound("rock", getComputerChoice()));
+paperBtn.addEventListener("click", ()=>playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener("click", ()=>playRound("scissors", getComputerChoice()));
+
+function playRound(humanChoice, computerChoice){
+    let roundResult = "";
+
+    if (computerScore >= 5 || humanScore >= 5){
+        computerScore = 0;
+        humanScore = 0;
     }
 
-    console.log("Human Score: ", humanScore)
-    console.log("Computer Score: ", computerScore)
-
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice == computerChoice){
-            return "It's a Tie"
-        } else if (humanChoice == "rock" && computerChoice == "scissors"){
-            humanScore += 1
-            return "You win! Rock beats Scissors"
-        } else if (humanChoice == "rock" && computerChoice == "paper"){
-            computerScore += 1
-            return "You lose! Paper beats Rock"
-        } else if (humanChoice == "paper" && computerChoice == "rock"){
-            humanScore += 1
-            return "You win! Paper beats Rock"
-        } else if (humanChoice == "paper" && computerChoice == "scissors"){
-            computerScore += 1
-            return "You lose! Scissors beats Paper"
-        } else if (humanChoice == "scissors" && computerChoice == "paper"){
-            humanScore += 1
-            return "You win! Scissors beats Paper"
-        } else if (humanChoice == "scissors" && computerChoice == "rock"){
-            return "You lose! Rock beats Scissors"
-        }
+    if (humanChoice == computerChoice){
+        roundResult = "It's a Tie"
+    } else if (humanChoice == "rock" && computerChoice == "scissors"){
+        humanScore += 1
+        roundResult = "You win! Rock beats Scissors"
+    } else if (humanChoice == "rock" && computerChoice == "paper"){
+        computerScore += 1
+        roundResult = "You lose! Paper beats Rock"
+    } else if (humanChoice == "paper" && computerChoice == "rock"){
+        humanScore += 1
+        roundResult = "You win! Paper beats Rock"
+    } else if (humanChoice == "paper" && computerChoice == "scissors"){
+        computerScore += 1
+        roundResult = "You lose! Scissors beats Paper"
+    } else if (humanChoice == "scissors" && computerChoice == "paper"){
+        humanScore += 1
+        roundResult = "You win! Scissors beats Paper"
+    } else if (humanChoice == "scissors" && computerChoice == "rock"){
+        computerScore += 1
+        roundResult = "You lose! Rock beats Scissors"
     }
-}
 
+    if (computerScore == 5){
+        roundResult = "The computer has bested you!";
+    } else if (humanScore == 5){
+        roundResult = "You have bested the computer!";
+    }
 
-function getHumanChoice(){
-    let inp = prompt("Rock Paper Scissors: ")
-    inp = inp.toLowerCase()
-    return inp;
+    computerScoreText.innerText = computerScore;
+    humanScoreText.innerText = humanScore;
+    output.innerText = roundResult;
+
 }
 
 function getComputerChoice(){
